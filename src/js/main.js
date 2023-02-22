02db678268b8945423fe0233831e684e6755f690
pratyush-eightfold <pratyush.gupta@eightfold.ai>
2023-02-22 23:38:01 +0530

    define the boundary for movement of the lift object

--- src/js/main.js
+++ src/js/main.js
@@ -5,19 +5,23 @@ lift.style.padding = "4px";
 lift.style.height = "100px";
 lift.style.width = "100px";
 lift.style.position = "absolute";
-lift.style.top = "200px";
-lift.style.left = "100px";
+lift.style.top = "150px";
+lift.style.left = "250px";
 
 lift.setAttribute("id", "liftObject");
 
 function moveLiftUp() {
   var lift = document.getElementById("liftObject");
-  lift.style.top = lift.offsetTop + 10 + "px";
+  if (lift.offsetTop < 600) {
+    lift.style.top = lift.offsetTop + 50 + "px";
+  }
 }
 
 function moveLiftDown() {
   var lift = document.getElementById("liftObject");
-  lift.style.top = lift.offsetTop - 10 + "px";
+  if (lift.offsetTop > 150) {
+    lift.style.top = lift.offsetTop - 50 + "px";
+  }
 }
 
 document.body.appendChild(lift);
